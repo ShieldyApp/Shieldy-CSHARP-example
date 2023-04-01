5c14bfd0b6b0a1eb84cfc87fcf0d89891a8a6d12
Kaspek2480 <matipole@gmail.com>
2023-04-01 13:33:54 +0200

    Fixed initialize check

--- ShieldyCSharpExample/ShieldyApi.cs
+++ ShieldyCSharpExample/ShieldyApi.cs
@@ -29,7 +29,7 @@ namespace ShieldyCSharpExample
                 return result.ToString();
             }
 
-            public static string Xor(string val, List<byte> key)
+            public static string Xor(string val, IList<byte> key)
             {
                 var result = new StringBuilder();
                 for (var i = 0; i < val.Length; i++)
@@ -63,8 +63,8 @@ namespace ShieldyCSharpExample
             {
                 if (a.Count != b.Count)
                     return false;
-                int i = 0;
-                foreach (byte c in a)
+                var i = 0;
+                foreach (var c in a)
                 {
                     if (c != b[i])
                         return false;
@@ -79,65 +79,75 @@ namespace ShieldyCSharpExample
                 // encoded OID sequence for  PKCS #1 rsaEncryption szOID_RSA_RSA = "1.2.840.113549.1.1.1"
                 byte[] seqOid =
                     { 0x30, 0x0D, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x01, 0x01, 0x05, 0x00 };
-                byte[] seq;
                 // ---------  Set up stream to read the asn.1 encoded SubjectPublicKeyInfo blob  ------
-                MemoryStream mem = new MemoryStream(x509Key);
-                BinaryReader binr = new BinaryReader(mem); //wrap Memory Stream with BinaryReader for easy reading
+                var memoryStream = new MemoryStream(x509Key);
+                var binaryReader = new BinaryReader(memoryStream); //wrap Memory Stream with BinaryReader for easy reading
 
                 try
                 {
-                    var twobytes = binr.ReadUInt16();
-                    if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
-                        binr.ReadByte(); //advance 1 byte
-                    else if (twobytes == 0x8230)
-                        binr.ReadInt16(); //advance 2 bytes
-                    else
-                        return null;
-
-                    seq = binr.ReadBytes(15); //read the Sequence OID
-                    if (!CompareBytearrays(seq, seqOid)) //make sure Sequence for OID is correct
-                        return null;
-
-                    twobytes = binr.ReadUInt16();
-                    if (twobytes ==
-                        0x8103) //data read as little endian order (actual data order for Bit String is 03 81)
-                        binr.ReadByte(); //advance 1 byte
-                    else if (twobytes == 0x8203)
-                        binr.ReadInt16(); //advance 2 bytes
-                    else
-                        return null;
-
-                    var bt = binr.ReadByte();
-                    if (bt != 0x00) //expect null byte next
-                        return null;
-
-                    twobytes = binr.ReadUInt16();
+                    var twobytes = binaryReader.ReadUInt16();
                     switch (twobytes)
                     {
                         //data read as little endian order (actual data order for Sequence is 30 81)
                         case 0x8130:
-                            binr.ReadByte(); //advance 1 byte
+                            binaryReader.ReadByte(); //advance 1 byte
                             break;
                         case 0x8230:
-                            binr.ReadInt16(); //advance 2 bytes
+                            binaryReader.ReadInt16(); //advance 2 bytes
                             break;
                         default:
                             return null;
                     }
 
-                    twobytes = binr.ReadUInt16();
-                    byte lowbyte = 0x00;
+                    var seq = binaryReader.ReadBytes(15);
+                    if (!CompareBytearrays(seq, seqOid)) //make sure Sequence for OID is correct
+                        return null;
+
+                    twobytes = binaryReader.ReadUInt16();
+                    switch (twobytes)
+                    {
+                        //data read as little endian order (actual data order for Bit String is 03 81)
+                        case 0x8103:
+                            binaryReader.ReadByte(); //advance 1 byte
+                            break;
+                        case 0x8203:
+                            binaryReader.ReadInt16(); //advance 2 bytes
+                            break;
+                        default:
+                            return null;
+                    }
+
+                    var bt = binaryReader.ReadByte();
+                    if (bt != 0x00) //expect null byte next
+                        return null;
+
+                    twobytes = binaryReader.ReadUInt16();
+                    switch (twobytes)
+                    {
+                        //data read as little endian order (actual data order for Sequence is 30 81)
+                        case 0x8130:
+                            binaryReader.ReadByte(); //advance 1 byte
+                            break;
+                        case 0x8230:
+                            binaryReader.ReadInt16(); //advance 2 bytes
+                            break;
+                        default:
+                            return null;
+                    }
+
+                    twobytes = binaryReader.ReadUInt16();
+                    byte lowbyte;
                     byte highbyte = 0x00;
 
                     switch (twobytes)
                     {
                         //data read as little endian order (actual data order for Integer is 02 81)
                         case 0x8102:
-                            lowbyte = binr.ReadByte(); // read next bytes which is bytes in modulus
+                            lowbyte = binaryReader.ReadByte(); // read next bytes which is bytes in modulus
                             break;
                         case 0x8202:
-                            highbyte = binr.ReadByte(); //advance 2 bytes
-                            lowbyte = binr.ReadByte();
+                            highbyte = binaryReader.ReadByte(); //advance 2 bytes
+                            lowbyte = binaryReader.ReadByte();
                             break;
                         default:
                             return null;
@@ -147,34 +157,34 @@ namespace ShieldyCSharpExample
                         { lowbyte, highbyte, 0x00, 0x00 }; //reverse byte order since asn.1 key uses big endian order
                     var modsize = BitConverter.ToInt32(modint, 0);
 
-                    var firstbyte = binr.ReadByte();
-                    binr.BaseStream.Seek(-1, SeekOrigin.Current);
+                    var firstbyte = binaryReader.ReadByte();
+                    binaryReader.BaseStream.Seek(-1, SeekOrigin.Current);
 
                     if (firstbyte == 0x00)
                     {
                         //if first byte (highest order) of modulus is zero, don't include it
-                        binr.ReadByte(); //skip this null byte
+                        binaryReader.ReadByte(); //skip this null byte
                         modsize -= 1; //reduce modulus buffer size by 1
                     }
 
-                    var modulus = binr.ReadBytes(modsize); //read the modulus bytes
+                    var modulus = binaryReader.ReadBytes(modsize); //read the modulus bytes
 
-                    if (binr.ReadByte() != 0x02) //expect an Integer for the exponent data
+                    if (binaryReader.ReadByte() != 0x02) //expect an Integer for the exponent data
                         return null;
                     var expbytes =
-                        (int)binr
+                        (int)binaryReader
                             .ReadByte(); // should only need one byte for actual exponent data (for all useful values)
-                    var exponent = binr.ReadBytes(expbytes);
+                    var exponent = binaryReader.ReadBytes(expbytes);
 
                     // ------- create RSACryptoServiceProvider instance and initialize with public key -----
-                    RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                    var rsaCryptoServiceProvider = new RSACryptoServiceProvider();
                     var rsaKeyInfo = new RSAParameters
                     {
                         Modulus = modulus,
                         Exponent = exponent
                     };
-                    rsa.ImportParameters(rsaKeyInfo);
-                    return rsa;
+                    rsaCryptoServiceProvider.ImportParameters(rsaKeyInfo);
+                    return rsaCryptoServiceProvider;
                 }
                 catch (Exception)
                 {
@@ -183,14 +193,14 @@ namespace ShieldyCSharpExample
 
                 finally
                 {
-                    binr.Close();
+                    binaryReader.Close();
                 }
             }
 
             public static bool Verify()
             {
                 PerformUpdate();
-                byte[] buff = File.ReadAllBytes(DllFilePath);
+                var buff = File.ReadAllBytes(DllFilePath);
 
                 //get last 256 bytes
                 var last256 = new byte[256];
@@ -200,8 +210,8 @@ namespace ShieldyCSharpExample
                 }
 
                 //get buff without last 256 bytes
-                byte[] buffWithoutLast256 = new byte[buff.Length - 256];
-                for (int i = 0; i < buffWithoutLast256.Length; i++)
+                var buffWithoutLast256 = new byte[buff.Length - 256];
+                for (var i = 0; i < buffWithoutLast256.Length; i++)
                 {
                     buffWithoutLast256[i] = buff[i];
                 }
@@ -210,11 +220,11 @@ namespace ShieldyCSharpExample
                 var md5 = MD5.Create().ComputeHash(buffWithoutLast256);
                 //print md5
 
-                RSACryptoServiceProvider rsa = DecodeX509PublicKey(Convert.FromBase64String(
+                var rsaCryptoServiceProvider = DecodeX509PublicKey(Convert.FromBase64String(
                     "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgb7m2HrJ7M6aiC9VzIOizWZ/XlB0eXSC56W6/ql5pUUjd0rEst6NgN1WuNlgjIjgqaRCIT2cJsX8yekjNSxwCogGcGTKm50i9ueh8SdwXtqIRMe4MHBuGbhimLlzDXhFGCfl8HIl2KpnyzBuIDqmuwNqJFdADXprHLiv066M6P9WKp8S4oIb0Y0s8k7aif7B/4bxHNe6ukI2uvVmAM0hEfq5g1pm2jvvAU9xytv2yWuYQ6u+0SzWkRAlP0MDKV9WsE/AKo9wID+Iod0u9U8tEj6HkiUhQ0V/q0BKjSWGOEUyujVoacVgswLOQU6nVdnntJEoZ9Jf8mOnbyLc6xTDTwIDAQAB"));
 
                 //verify
-                if (rsa.VerifyData(md5, CryptoConfig.MapNameToOID("SHA256"), last256)) return true;
+                if (rsaCryptoServiceProvider.VerifyData(md5, CryptoConfig.MapNameToOID("SHA256"), last256)) return true;
 
                 if (Utils.DebugMode) Console.WriteLine("Verification of dll failed.");
                 Environment.Exit(0);
@@ -267,25 +277,24 @@ namespace ShieldyCSharpExample
         public static bool Initialize(string appGuid, string version, string appSalt)
         {
             Utils.AppSalt = appSalt;
-
             if (!Utils.Verify()) return false;
-            return Bindings.SC_Initialize(appGuid, version);
+
+            Utils.Initialized = Bindings.SC_Initialize(appGuid, version);
+            return Utils.Initialized;
         }
 
         public static bool Login(string username, string password)
         {
-            if (!Bindings.SC_LoginUserPass(username, password)) return false;
-
-            Utils.Initialized = true;
-            return true;
+            return Bindings.SC_LoginUserPass(username, password);
         }
 
         public static bool Login(string licenseKey)
         {
-            if (!Bindings.SC_LoginLicenseKey(licenseKey)) return false;
+            if (Utils.Initialized) return Bindings.SC_LoginLicenseKey(licenseKey);
+            
+            if (Utils.DebugMode) Console.WriteLine("You must initialize the library before logging in.");
+            return false;
 
-            Utils.Initialized = true;
-            return true;
         }
 
         public static int GetLastError()
@@ -295,12 +304,16 @@ namespace ShieldyCSharpExample
 
         public static string GetVariable(string name)
         {
-            IntPtr buf;
-            int size;
-            bool result = Bindings.SC_GetVariable(name, out buf, out size);
+            if (!Utils.Initialized)
+            {
+                if (Utils.DebugMode) Console.WriteLine("You must initialize the library before logging in.");
+                return "";
+            }
+            
+            var result = Bindings.SC_GetVariable(name, out var buf, out var size);
             if (result)
             {
-                byte[] data = new byte[size];
+                var data = new byte[size];
                 Marshal.Copy(buf, data, 0, size);
                 var resultVariable = Encoding.UTF8.GetString(data);
 
@@ -318,12 +331,16 @@ namespace ShieldyCSharpExample
 
         public static string GetUserProperty(string propertyName)
         {
-            IntPtr buf;
-            int size;
-            bool result = Bindings.SC_GetUserProperty(propertyName, out buf, out size);
+            if (!Utils.Initialized)
+            {
+                if (Utils.DebugMode) Console.WriteLine("You must initialize the library before logging in.");
+                return "";
+            }
+            
+            var result = Bindings.SC_GetUserProperty(propertyName, out var buf, out var size);
             if (result)
             {
-                byte[] data = new byte[size];
+                var data = new byte[size];
                 Marshal.Copy(buf, data, 0, size);
                 var resultVariable = Encoding.UTF8.GetString(data);
 
@@ -342,12 +359,16 @@ namespace ShieldyCSharpExample
 
         public static string DeobfuscateString(string base64, int rounds)
         {
-            IntPtr buf;
-            int size;
-            bool result = Bindings.SC_DeobfString(base64, rounds, out buf, out size);
+            if (!Utils.Initialized)
+            {
+                if (Utils.DebugMode) Console.WriteLine("You must initialize the library before logging in.");
+                return "";
+            }
+
+            var result = Bindings.SC_DeobfString(base64, rounds, out var buf, out var size);
             if (result)
             {
-                byte[] data = new byte[size];
+                var data = new byte[size];
                 Marshal.Copy(buf, data, 0, size);
                 var resultVariable = Encoding.UTF8.GetString(data);
 
@@ -366,12 +387,16 @@ namespace ShieldyCSharpExample
 
         public static List<byte> DownloadFile(string name)
         {
-            IntPtr buf;
-            int size;
-            bool result = Bindings.SC_DownloadFile(name, out buf, out size);
+            if (!Utils.Initialized)
+            {
+                if (Utils.DebugMode) Console.WriteLine("You must initialize the library before logging in.");
+                return null;
+            }
+
+            var result = Bindings.SC_DownloadFile(name, out var buf, out var size);
             if (result)
             {
-                byte[] data = new byte[size];
+                var data = new byte[size];
                 Marshal.Copy(buf, data, 0, size);
 
                 if (!Bindings.SC_FreeMemory(out buf) && Utils.DebugMode)
